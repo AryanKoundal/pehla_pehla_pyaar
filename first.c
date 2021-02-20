c0dcb1f53f75a4ea500578393b33ee1b8348cb93
AryanKoundal <77334487+AryanKoundal@users.noreply.github.com>
2021-02-21 00:24:45 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+/*
+yeh mera pehla c program hai, lagi padi hai
+*/
+int main(){
+    // Declaring variables to store error codes
+    int branchone;
+    int empty;
+    int tom;
+    int TOM;
+    int error_code;
+    // '@' --> a character
+    printf("kuch ni samajh aa rha");
+    return 0;
+}
